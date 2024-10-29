7d5c13c0838ab73147ce0a14f6e8cc4b4f04d7de
huahua20414 <2041436630@qq.com>
2024-10-29 18:32:25 +0800

    配置k8s-mysql和k8s-redis

--- day01/main.go
+++ day01/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"go-study/day01/internal/web"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	server := web.RegisterRoutes()
+	//server := web.RegisterRoutes()
+	server := gin.Default()
+	server.GET("/login", func(c *gin.Context) {
+		c.String(200, "成功")
+	})
 	server.Run(":8080")
 }
